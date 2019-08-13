e3c50ba18f0b835a49f54e18a1f27f9a920096f7
Charlie Afif <charlieafif@gmail.com>
2019-08-13 08:17:13 +0100

    added basic routes and initialised express

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 const express = require('express')
 const app = express()
 
+
+let allowCrossDomain = function(req, res, next) {
+	res.header('Access-Control-Allow-Origin', "*");
+	res.header('Access-Control-Allow-Headers', "*");
+	next();
+}
+app.use(allowCrossDomain);
+
+app.use(express.json())
+
+app.get('/users', (req, res) => {
+	res.send('testing')
+})
+
 app.post('/users', (req, res) => {
-    res.send('testing')
+	res.status(201)
+	console.log(req.body)
+	res.send(req.body)
 })
 
 app.listen(3000, () => {
